dbd70744655fa24a680daab28c3635607a59c769
MircoT <mirco.theone@gmail.com>
2018-09-11 17:32:45 +0200

    Add shuffle service

--- configure_spark.sh
+++ configure_spark.sh
@@ -8,3 +8,4 @@ echo -e "\n" >> /opt/spark/conf/spark-defaults.conf
 MESOS_MASTER=$(mesos_master4spark)
 echo "==> Add Mesos master [$MESOS_MASTER] in Spark conf"
 echo -e "spark.master\t$MESOS_MASTER\n" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.shuffle.service.enabled\ttrue\n" >> /opt/spark/conf/spark-defaults.conf
