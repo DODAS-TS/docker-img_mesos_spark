ddccf6d569e31e7688b12f3b41aab3dc1faaf613
MircoT <mirco.theone@gmail.com>
2018-09-11 10:56:04 +0200

    Add configure spark script

---
+++ configure_spark.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "==> Prepare Spark conf"
+cp /opt/spark/conf/spark-defaults.conf.template /opt/spark/conf/spark-defaults.conf
+echo "\n" >> /opt/spark/conf/spark-defaults.conf
+MESOS_MASTER=$(mesos_master4spark)
+echo "==> Add Mesos master [$MESOS_MASTER] in Spark conf"
+echo "spark.master\t$MESOS_MASTER\n" >> /opt/spark/conf/spark-defaults.conf
