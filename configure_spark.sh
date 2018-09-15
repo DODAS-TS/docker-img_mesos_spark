6975091e236f3e06cf5bb9fc2b05a06d0ed4022c
MircoT <mirco.theone@gmail.com>
2018-09-15 16:29:23 +0200

    Update spark base configuration file

--- configure_spark.sh
+++ configure_spark.sh
@@ -9,3 +9,7 @@ MESOS_MASTER=$(mesos_master4spark)
 echo "==> Add Mesos master [$MESOS_MASTER] in Spark conf"
 echo -e "spark.master\t$MESOS_MASTER" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.shuffle.service.enabled\ttrue" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.executor.memory\t512m" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.executor.cores\t1" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.executor.cores.max\t1" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.cores.max\t2" >> /opt/spark/conf/spark-defaults.conf
