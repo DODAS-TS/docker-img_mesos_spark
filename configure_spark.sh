4a4c5d4d9efeca94167931b3bbb4b640426f7511
MircoT <mirco.theone@gmail.com>
2018-09-27 13:16:19 +0200

    Add spark.dynamicAllocation.enabled

--- configure_spark.sh
+++ configure_spark.sh
@@ -8,6 +8,7 @@ echo -e "\n" >> /opt/spark/conf/spark-defaults.conf
 MESOS_MASTER=$(mesos_master4spark)
 echo "==> Add Mesos master [$MESOS_MASTER] in Spark conf"
 echo -e "spark.master\t$MESOS_MASTER" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.dynamicAllocation.enabled\ttrue" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.shuffle.service.enabled\ttrue" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.executor.memory\t512m" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.executor.cores\t1" >> /opt/spark/conf/spark-defaults.conf
