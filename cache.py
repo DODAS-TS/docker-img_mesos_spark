e685303ac6712896528989b335b24b133f8bafb2
MircoT <MircoT@users.noreply.github.com>
2019-01-11 13:18:03 +0100

    Update cache.py to python 3

--- cache.py
+++ cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Module for caching variables in different stores."""
 import argparse
@@ -266,7 +266,10 @@ class ZookeeperCache(CacheManager):
         try:
             logging.debug("Zookeeper GET variable %s", name)
             container, _ = self.zk_client.get(self.map_[name])
-            value = json.loads(container).get('val')
+            if PY2:
+                value = json.loads(container).get('val')
+            else:
+                value = json.loads(container.decode('utf-8')).get('val')
         except kazoo_exceptions.NoNodeError:
             return "ERROR: Node NOT EXISTS or was DELETED!"
         return value
