40c7e001a84c69bc0d4518cf17e7c788d6740af5
MircoT <MircoT@users.noreply.github.com>
2019-01-25 15:23:58 +0100

    Fix spark max core

--- configure_spark.sh
+++ configure_spark.sh
@@ -13,7 +13,7 @@ echo -e "spark.shuffle.service.enabled\ttrue" >> /opt/spark/conf/spark-defaults.
 echo -e "spark.executor.memory\t512m" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.executor.cores\t1" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.executor.cores.max\t1" >> /opt/spark/conf/spark-defaults.conf
-echo -e "spark.cores.max\t2" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.cores.max\t1" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.mesos.executor.docker.image\tindigodatacloudapps/mesos-spark:base" >> /opt/spark/conf/spark-defaults.conf
 ecgi -e "spark.mesos.executor.docker.forcePullImage\ttrue" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.shuffle.reduceLocality.enabled\tfalse" >> /opt/spark/conf/spark-defaults.conf
