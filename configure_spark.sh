808b9b4a9a666ef877c1046ed0148dae3ecb2b4a
MircoT <MircoT@users.noreply.github.com>
2019-02-25 14:44:35 +0100

    Change base configuration of spark

--- configure_spark.sh
+++ configure_spark.sh
@@ -10,14 +10,14 @@ echo "==> Add Mesos master [$MESOS_MASTER] in Spark conf"
 echo -e "spark.master\t$MESOS_MASTER" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.dynamicAllocation.enabled\ttrue" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.shuffle.service.enabled\ttrue" >> /opt/spark/conf/spark-defaults.conf
-echo -e "spark.executor.memory\t1g" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.executor.memory\t512g" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.mesos.executor.memoryOverhead\t128" >> /opt/spark/conf/spark-defaults.conf
-echo -e "spark.driver.memory\t2g" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.driver.memory\t1g" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.driver.memoryOverhead\t512" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.executor.cores\t1" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.executor.cores.max\t1" >> /opt/spark/conf/spark-defaults.conf
-echo -e "spark.cores.max\t1" >> /opt/spark/conf/spark-defaults.conf
-echo -e "spark.mesos.driver.failoverTimeout\t60.0" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.cores.max\t2" >> /opt/spark/conf/spark-defaults.conf
+echo -e "spark.mesos.driver.failoverTimeout\t300.0" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.mesos.executor.docker.image\tindigodatacloudapps/mesos-spark:base" >> /opt/spark/conf/spark-defaults.conf
 echo -e "# spark.mesos.executor.docker.forcePullImage\ttrue" >> /opt/spark/conf/spark-defaults.conf
 echo -e "spark.shuffle.reduceLocality.enabled\tfalse" >> /opt/spark/conf/spark-defaults.conf
