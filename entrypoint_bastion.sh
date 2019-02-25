09b0fdde5ec28f92a98dbbe811a1d7e859a81649
MircoT <MircoT@users.noreply.github.com>
2019-02-25 16:24:28 +0100

    Add jupyter and spark proxy hosts

--- entrypoint_bastion.sh
+++ entrypoint_bastion.sh
@@ -14,5 +14,9 @@ chown -R admin:admin /home/admin/.ssh
 export NETWORK_INTERFACE=$(hostname -i)
 echo "==> Public target host"
 dodas_cache zookeeper TARGET_HOST "$NETWORK_INTERFACE"
+echo "==> Public SPARK proxy host"
+dodas_cache zookeeper SPARK_PROXY_TARGET_HOST "$NETWORK_INTERFACE"
+echo "==> Public JUPYTER proxy host"
+dodas_cache zookeeper JUPYTER_PROXY_TARGET_HOST "$NETWORK_INTERFACE"
 echo "==> Start sshd on port $TARGET_SSH_PORT"
 exec /usr/sbin/sshd -E /var/log/sshd.log -g 30 -p $TARGET_SSH_PORT -D
