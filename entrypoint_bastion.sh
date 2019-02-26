06e9e47719b1ca0561d35475b02290df20b5d670
MircoT <MircoT@users.noreply.github.com>
2019-02-26 16:24:11 +0100

    Fix pyspark shell with jupyter

--- entrypoint_bastion.sh
+++ entrypoint_bastion.sh
@@ -14,28 +14,32 @@ chown -R admin:admin /home/admin/.ssh
 export NETWORK_INTERFACE=$(hostname -i)
 echo "==> Public target host"
 dodas_cache zookeeper TARGET_HOST "$NETWORK_INTERFACE"
-echo "\n==> Public SPARK proxy host"
+echo -e "\n==> Public SPARK proxy host"
 dodas_cache zookeeper SPARK_PROXY_TARGET_HOST "$NETWORK_INTERFACE"
-echo "\n==> Public JUPYTER proxy host"
+echo -e "\n==> Public JUPYTER proxy host"
 dodas_cache zookeeper JUPYTER_PROXY_TARGET_HOST "$NETWORK_INTERFACE"
 
 if [ "$CONTAINER_TARGET" == "SSH" ] ; 
 then
-    echo "\n==> Start sshd on port $TARGET_SSH_PORT"
+    echo -e "\n==> Start sshd on port $TARGET_SSH_PORT"
     exec /usr/sbin/sshd -E /var/log/sshd.log -g 30 -p $TARGET_SSH_PORT -D
 elif [ "$CONTAINER_TARGET" == "JUPYTER" ]
 then
-    echo "==> Prepare jupyter environment"
+    echo -e "\n==> Prepare jupyter environment"
     mkdir -p /root/.jupyter
+    mkdir -p /root/jupyter
     echo "c.NotebookApp.allow_origin = '*'" >> /root/.jupyter/jupyter_notebook_config.py
     echo "c.NotebookApp.allow_root = True " >> /root/.jupyter/jupyter_notebook_config.py
     echo "c.NotebookApp.ip = '0.0.0.0'" >> /root/.jupyter/jupyter_notebook_config.py
     echo "c.NotebookApp.open_browser = False" >> /root/.jupyter/jupyter_notebook_config.py
-    JUPYTER_PASSWORD_HASH=`python3 -c 'from notebook.auth import passwd; print(passwd($JUPYTER_PASSWORD))'`
+    export GEN_JUPYTER_PASS_CMD="from notebook.auth import passwd; print(passwd('${JUPYTER_PASSWORD}'))"
+    JUPYTER_PASSWORD_HASH=$(python3 -c "${GEN_JUPYTER_PASS_CMD}")
+    unset GEN_JUPYTER_PASS_CMD
     echo "c.NotebookApp.password = u'$JUPYTER_PASSWORD_HASH'" >> /root/.jupyter/jupyter_notebook_config.py
     export PYSPARK_DRIVER_PYTHON='jupyter'
     export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
     echo "==> Start jupyter"
+    cd /root/jupyter
     exec pyspark --properties-file /opt/spark/conf/spark-defaults.conf
 else
     echo "Target $CONTAINER_TARGET is not implemented..."
